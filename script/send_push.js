77d590017f29f8206c2778c86ca5b417a4fedd00
Mark Terry <mark.terry@holidayextras.com>
2016-10-27 15:53:41 +0100

    now using correct config and git url updated

--- script/send_push.js
+++ script/send_push.js
@@ -5,8 +5,7 @@ var SumoLogger = require('sumologic');
 var config = require('../src/config');
 
 // Set up logging for production.
-if (process.env.ENV &&
-    process.env.ENV === 'production') {
+if (config.ENV === 'production') {
   var logger = new SumoLogger(config.SUMOLOGIC_CODE, {
     endpoint: config.SUMOLOGIC_URL,
     syncInterval: 1000
