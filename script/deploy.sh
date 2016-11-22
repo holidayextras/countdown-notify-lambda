c5935aee8ea75f13582a60a2bc464885edc42f4a
Mark Terry <mark.terry@holidayextras.com>
2016-11-22 11:25:18 +0000

    fixed typos in deploy script

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Travis branch: ${TRAVIS_BRANCH}";
-if [ "${TRAVIS_BRANCH}" = "master"]; then
+if [ "${TRAVIS_BRANCH}" = "master" ]; then
   NODE_ENV=production
   AWS_ACCESS_KEY_ID=${PRODUCTION_AWS_ACCESS_KEY_ID}
   AWS_SECRET_ACCESS_KEY=${PRODUCTION_AWS_SECRET_ACCESS_KEY}
@@ -10,7 +10,7 @@ else
   NODE_ENV=staging
   AWS_ACCESS_KEY_ID=${STAGING_AWS_ACCESS_KEY_ID}
   AWS_SECRET_ACCESS_KEY=${STAGING_AWS_SECRET_ACCESS_KEY}
-if
+fi
 
 echo "Using ${NODE_ENV} settings";
 
