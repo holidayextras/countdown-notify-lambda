a8da490900079a0b6dd96f01346bb100879a8a99
Mark Terry <mark.terry@holidayextras.com>
2016-11-22 11:07:51 +0000

    now setting the correct AWS creds

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 set -e
 
+echo "Travis branch: ${TRAVIS_BRANCH}";
+if [ "${TRAVIS_BRANCH}" = "master"]; then
+  NODE_ENV=production
+  AWS_ACCESS_KEY_ID=${PRODUCTION_AWS_ACCESS_KEY_ID}
+  AWS_SECRET_ACCESS_KEY=${PRODUCTION_AWS_SECRET_ACCESS_KEY}
+else
+  NODE_ENV=staging
+  AWS_ACCESS_KEY_ID=${STAGING_AWS_ACCESS_KEY_ID}
+  AWS_SECRET_ACCESS_KEY=${STAGING_AWS_SECRET_ACCESS_KEY}
+if
+
+echo "Using ${NODE_ENV} settings";
+
 npm run deploy
